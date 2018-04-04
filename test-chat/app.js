cdd920b5c1fe9d26f5fd074fb19bea8cf5129593
Tanguy Roudaut <tanguy.roudaut@telecomnancy.net>
2018-04-04 16:46:49 +0100

    chat en local

--- test-chat/app.js
+++ test-chat/app.js
@@ -1,27 +1,48 @@
+var express = require('express');
 var app = require('express')(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+    ent = require('ent'), // Permet de bloquer les caractÃ¨res HTML (sÃ©curitÃ© Ã©quivalente Ã  htmlentities en PHP)
     fs = require('fs');
 
 // Chargement de la page index.html
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  app.use(express.static(__dirname+'/public'));
+  res.sendFile(__dirname + '\\index.html');
 });
 
-io.sockets.on('connection', function (socket, pseudo) {
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', function(pseudo) {
-        pseudo = ent.encode(pseudo);
-        socket.pseudo = pseudo;
-        socket.broadcast.emit('nouveau_client', pseudo);
+var users= {};
+
+io.sockets.on('connection', function (socket) {
+    // DÃ¨s qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    var me=false;
+    for(var k in users){
+      socket.emit('nouveau_client',users[k]);
+    }
+
+    socket.on('login', function(user) {
+        me=user;
+        me.username=ent.encode(me.username);
+        me.id="id"+ent.encode(user.username);
+        users[me.id]=me;
+        io.sockets.emit('nouveau_client', me);
     });
 
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    // DÃ¨s qu'on reÃ§oit un message, on rÃ©cupÃ¨re le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
-        message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-    }); 
+        message.content=ent.encode(message.content);
+        io.sockets.emit('msg', message);
+    });
+
+    socket.on('disconnect',function(){
+        if(!me){
+          return false;
+        }
+        delete users[me.id];
+        io.sockets.emit('deconnexion_client',me);
+      });
 });
 
-server.listen(80,193.54.15.211);
+
+
+server.listen(8080);
