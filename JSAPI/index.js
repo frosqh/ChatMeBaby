2b8ed391a3b43e89667cf016887f73fe0ce122fb
JeremyHynes <jeremy.hynes@telecomnancy.net>
2018-04-04 20:28:01 +0200

    Zgeg

---
+++ JSAPI/index.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.facebook').on("click",function(){
+    window.open('https://www.facebook.com/Chat-Me-Baby-130671114443167/', '_blank');
+    });
+  $('.twitter').on("click",function(){
+    window.open('https://twitter.com/JeremyHynes4', '_blank');
+    });
+  $('.linkedin').on("click",function(){
+    window.open('http://www.linkedin.com/in/jeremy-hynes-6bab77146', '_blank');
+    });
+});
