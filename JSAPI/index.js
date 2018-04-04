af494039216bfb881bc33c964211abbe02863570
JeremyHynes <jeremy.hynes@telecomnancy.net>
2018-04-04 21:20:34 +0200

    corrections

--- JSAPI/index.js
+++ JSAPI/index.js
@@ -1,4 +1,13 @@
 $(function(){
+  $('#homelink').on("click",function(){
+    window.location.href = "home.html";
+    });
+  $('#channelbutton').on("click",function(){
+    window.location.href = "channel.html";
+  });
+  $('#settingico').on("click",function(){
+    window.location.href = "profile.html";
+    });
   $('.facebook').on("click",function(){
     window.open('https://www.facebook.com/Chat-Me-Baby-130671114443167/', '_blank');
     });
