38537944a3ca05fe90e7fdbfbd586c434e02f187
frosqh <frosqh@gmail.com>
2018-04-05 17:19:02 +0200

    Correction img

--- static/JS/channel.js
+++ static/JS/channel.js
@@ -5,27 +5,7 @@ $('.channel').on( "click", function() {
   var newChannelName = $('.active_channel').text();
   date= new Date();
   $('.channelNam p b').html(newChannelName);
-  socket.emit('getMessages', {
-      channel: newChannelName
-    });
-  socket.on('messages', function(messages){
-	  console.log(messages);
-    $('#zone_chat').html('');
-   for (i in messages){
-	   m=messages[i];
-     time = m.SendDate.split(' ')[1];
-     minute= time[1];
-     hour = time[0];
-     lastMsg="";
-     if(lastMsg != message.user){
-          $('#zone_chat').append('<div class="sender-name"><img class="avatar" src="https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/29792871_130672044443074_3886540281580134733_n.png?_nc_cat=0&oh=ba0d45495f5506030849de77f27b16b6&oe=5B73A591" width="32" height="32"/><div class="sender">'+m.UserName+'</div><div class="time">'+hour+':'+minute+'</div><div class="message_content"><div class="message_body">  <strong>|</strong> ' + m.Txt + '</div></div></div>');
-          lastMsg = message.user;
-      } else {
-          $('#zone_chat').append('<div class="message_content"><div class="time">'+hour+':'+minute+'</div><div class="message_body">  <strong>|</strong> ' + m.Txt + '</div></div>');
-          $('.content').animate({scrollTop : $('.content').prop('scrollHeight')},500);
-      }
-   }
-  })
+  refresh(newChannelName)
   //socket.emit('getUser');
   //socket.on('user', function(user) {
   //  socket.emit('message', {
@@ -36,6 +16,26 @@ $('.channel').on( "click", function() {
   //  });
   //  console.log("channel.js",newChannelName);
   //});
-
-
 });
+
+function refresh(newChannelName){
+    socket.emit('getMessages', {
+      channel: newChannelName
+    });
+  socket.on('messages', function(messages){
+    $('#zone_chat').html('');
+    lastMsg="";
+   for (i in messages){
+     m=messages[i];
+     time = m.SendDate.split(' ')[1];
+     minute= time.split('-')[1];
+     hour = time.split('-')[0];
+     if(lastMsg != message.user){
+          $('#zone_chat').append('<div class="sender-name"><img class="avatar" src="https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/29792871_130672044443074_3886540281580134733_n.png?_nc_cat=0&oh=ba0d45495f5506030849de77f27b16b6&oe=5B73A591" width="32" height="32"/><div class="sender">'+m.UserName+'</div><div class="time">'+hour+':'+minute+'</div><div class="message_content"><div class="message_body">  <strong>|</strong> ' + m.Txt + '</div></div></div>');
+          lastMsg = message.user;
+      } else {
+          $('#zone_chat').append('<div class="message_content"><div class="time">'+hour+':'+minute+'</div><div class="message_body">  <strong>|</strong> ' + m.Txt + '</div></div>');
+          $('.content').animate({scrollTop : $('.content').prop('scrollHeight')},500);
+      }
+   }
+}
