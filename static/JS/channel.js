ff15ac665b05908176b2ad59ee3ce7cf4853d74e
frosqh <frosqh@gmail.com>
2018-04-05 17:24:24 +0200

    Correction img

--- static/JS/channel.js
+++ static/JS/channel.js
@@ -28,8 +28,8 @@ function refresh(newChannelName){
    for (i in messages){
      m=messages[i];
      time = m.SendDate.split(' ')[1];
-     minute= time.split('-')[1];
-     hour = time.split('-')[0];
+     minute= time.split(':')[1];
+     hour = time.split(':')[0];
      if(lastMsg != message.user){
           $('#zone_chat').append('<div class="sender-name"><img class="avatar" src="https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/29792871_130672044443074_3886540281580134733_n.png?_nc_cat=0&oh=ba0d45495f5506030849de77f27b16b6&oe=5B73A591" width="32" height="32"/><div class="sender">'+m.UserName+'</div><div class="time">'+hour+':'+minute+'</div><div class="message_content"><div class="message_body">  <strong>|</strong> ' + m.Txt + '</div></div></div>');
           lastMsg = message.user;
